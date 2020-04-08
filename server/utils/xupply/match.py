12e101d82a2f3de27f8a8f022c70f8e54e9740b2
Denis Angell <denisangell@harpangell.com>
2020-04-08 18:35:30 -0400

    Fixup Cloud Matching

--- server/utils/xupply/match.py
+++ server/utils/xupply/match.py
@@ -98,44 +98,44 @@ def cloud_menu_item_match(account):
 
     for mii in range(len(item_dict['items'])):
         request_item = item_dict['items'][ri]
-        request_itemID = request_item['itemID']
-        manu_item_stock = menu_item_stock[request_itemID]
-        print('MenuItem Item ID: {}'.format(request_itemID))
-        for account_itemID, stock_quantities in manu_item_stock.items():
-            print('Checking Account Item ID: {}'.format(account_itemID))
-            # Create New Order Object
-            new_order = {}
-            new_order['items'] = []
-            new_order['stockPerItem'] = {}
-            for rqi in range(len(request_item['quantities'])):
-                request_quantity = request_item['quantities'][rqi]
-                for sqi in range(len(stock_quantities)):
-                    stock_quantity = stock_quantities[sqi]
-                    if stock_quantity['packageType'] == request_quantity['packageType']:
-                        print('Quantity Found')
-                        print('Before - MenuItemed Menu Item Stock: {}'.format(request_quantity['stock']))
-                        print('Before - Manufacturer Menu Item Stock: {}'.format(stock_quantity['stock']))
-                        if int(request_quantity['stock']) <= int(stock_quantity['stock']):
-                            new_item = {
-                                'item': request_item,
-                                'quantity': request_quantity,
-                                'stock': int(request_quantity['stock'])
-                            }
-                            new_order['items'].append(new_item)
-                            new_order['stockPerItem'][request_item['itemID']] = {
-                                'type': request_quantity['packageType'],
-                                'quantity': request_quantity['packageQuantity'],
-                                'stock': int(request_quantity['stock'])
-                            }
-                            stock = int(stock_quantity['stock']) - int(request_quantity['stock'])
-                            manu_item_stock[account_itemID][sqi]['stock'] = stock
-                            item_dict['items'][ri]['quantities'][rqi]['stock'] = 0
-                            updated_menu_items.append(manu_item_stock)
-                            new_orders.append(new_order)
-                            updated_request = item_dict
+    request_itemID = item_dict['itemID']
+    manu_item_stock = menu_item_stock[request_itemID]
+    print('MenuItem Item ID: {}'.format(request_itemID))
+    for account_itemID, stock_quantities in manu_item_stock.items():
+        print('Checking Account Item ID: {}'.format(account_itemID))
+        # Create New Order Object
+        new_order = {}
+        new_order['items'] = []
+        new_order['stockPerItem'] = {}
+        for rqi in range(len(request_item['quantities'])):
+            request_quantity = request_item['quantities'][rqi]
+            for sqi in range(len(stock_quantities)):
+                stock_quantity = stock_quantities[sqi]
+                if stock_quantity['packageType'] == request_quantity['packageType']:
+                    print('Quantity Found')
+                    print('Before - MenuItemed Menu Item Stock: {}'.format(request_quantity['stock']))
+                    print('Before - Manufacturer Menu Item Stock: {}'.format(stock_quantity['stock']))
+                    if int(request_quantity['stock']) <= int(stock_quantity['stock']):
+                        new_item = {
+                            'item': request_item,
+                            'quantity': request_quantity,
+                            'stock': int(request_quantity['stock'])
+                        }
+                        new_order['items'].append(new_item)
+                        new_order['stockPerItem'][request_item['itemID']] = {
+                            'type': request_quantity['packageType'],
+                            'quantity': request_quantity['packageQuantity'],
+                            'stock': int(request_quantity['stock'])
+                        }
+                        stock = int(stock_quantity['stock']) - int(request_quantity['stock'])
+                        manu_item_stock[account_itemID][sqi]['stock'] = stock
+                        item_dict['items'][ri]['quantities'][rqi]['stock'] = 0
+                        updated_menu_items.append(manu_item_stock)
+                        new_orders.append(new_order)
+                        updated_request = item_dict
 
-            print('After - MenuItemed Menu Item Stock: {}'.format(item_dict['items'][ri]['quantities'][rqi]['stock']))
-            print('After - Manufacturer Menu Item Stock: {}'.format(manu_item_stock[account_itemID][sqi]['stock']))
+        print('After - MenuItemed Menu Item Stock: {}'.format(item_dict['items'][ri]['quantities'][rqi]['stock']))
+        print('After - Manufacturer Menu Item Stock: {}'.format(manu_item_stock[account_itemID][sqi]['stock']))
     print(updated_menu_items)
     print(new_orders)
     print(updated_request)
