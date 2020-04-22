7ecf19101f52ba5f8f971e5f173a218c18714e48
Denis Angell <denisangell@harpangell.com>
2020-04-22 14:45:26 -0400

    Remove Comments

--- static/src/utils/inventory.js
+++ static/src/utils/inventory.js
@@ -2,7 +2,7 @@
 
 // Calculate Over Burn Stock
 export function calculateOverBurnStock(quantity) {
-    console.error(quantity)
+    // console.error(quantity)
     if (!quantity || quantity.burnQuantity === 0 || quantity === {}) {
         return 'N/A';
     }
@@ -11,13 +11,13 @@ export function calculateOverBurnStock(quantity) {
     if (overBurn >= 0) {
         return 'N/A';
     }
-    console.error(overBurn)
+    // console.error(overBurn)
     return overBurn;
 }
 
 // Calculate Over Burn Priority
 export function calculateOverBurnPriority(overBurn) {
-    console.error(overBurn)
+    // console.error(overBurn)
     if (overBurn < 0) {
         return 'high';
     }
@@ -56,19 +56,19 @@ function range(start, stop, step) {
 
 // Calculate Over Burn Required By Date
 export function calculateOverBurnRequiredBy(quantity) {
-    console.error(quantity)
-    console.error(range(quantity.burnDays))
+    // console.error(quantity)
+    // console.error(range(quantity.burnDays))
     var daysToBurn = 0;
     var burnDate = new Date();
     var stock = quantity.stock;
     range(quantity.burnDays).forEach((d) => {
           var overBurn = stock - (quantity.burnQuantity * d); // index + 1 = day
-          console.log(overBurn)
+          // console.log(overBurn)
           if (overBurn < 0) {
               const today = new Date();
               const priorSeconds = new Date().setDate(today.getDate() + d);
               const burnDay = new Date(priorSeconds);
-              console.log(burnDate)
+              // console.log(burnDate)
               daysToBurn = d;
               burnDate = burnDay;
           }
@@ -79,25 +79,25 @@ export function calculateOverBurnRequiredBy(quantity) {
 
 // Calculate Lead Available By Date
 export function calculateLeadAvailableBy(quantity) {
-    console.error(quantity)
-    console.error(range(quantity.leadDays))
+    // console.error(quantity)
+    // console.error(range(quantity.leadDays))
     var daysToLead = 0;
     var leadStartTime = quantity.leadStartTime;
     var leadFinishTime = quantity.leadStartTime;
     var stock = quantity.stock;
     range(quantity.leadDays).forEach((d) => {
           var availLead = stock - (quantity.leadQuantity * d); // index + 1 = day
-          console.log(availLead)
+          // console.log(availLead)
           if (availLead > 0) {
               const today = new Date();
               const priorSeconds = new Date().setDate(today.getDate() + d);
               const leadDay = new Date(priorSeconds);
-              console.log(leadFinishTime)
+              // console.log(leadFinishTime)
               daysToLead = d;
               leadFinishTime = leadDay;
           }
     });
-    console.log(leadStartTime)
-    console.log(leadFinishTime)
+    // console.log(leadStartTime)
+    // console.log(leadFinishTime)
     return {leadStartTime, leadFinishTime}
 }
